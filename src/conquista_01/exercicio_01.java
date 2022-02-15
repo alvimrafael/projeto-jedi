9d40eec8e095384e4b0e1e6ff9fabf0ca9b15ba8
Rafael Alvim <rafael.alvimdossantos@gmail.com>
2022-02-15 17:28:54 -0300

    exercicio_01

---
+++ src/conquista_01/exercicio_01.java
@@ -0,0 +1,16 @@
+package conquista_01;
+
+/* Declarar uma variável int numero dentro do escopo do método main
+e na linha seguinte atribuir um valor para ela.
+Após isso, usar o método System.out.println() para exibir o valor da variável no console.
+OBS*: Se atentar ao parâmetro do método de exibição */
+
+public class exercicio_01 {
+    public static void main (String[] args) { // method - opening
+
+        int numero; // creating local variable
+        numero = 13; // assigning value
+        System.out.println(numero);
+
+    } // method - closing
+}
