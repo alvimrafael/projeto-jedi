ae2eefb094c3cb735475c595b8bb61f07737211b
Rafael Alvim <rafael.alvimdossantos@gmail.com>
2022-02-15 17:59:33 -0300

    exercicio_03

---
+++ src/conquista_01/exercicio_03.java
@@ -0,0 +1,15 @@
+package conquista_01;
+
+// Criar um código dentro do método main que soma dois números e exibe o valor ao final do programa.
+
+public class exercicio_03 {
+
+    public static void main (String[] args) {
+
+        int a, b;
+        a = 6;
+        b = 7;
+
+        System.out.println(a+b);
+    }
+}
