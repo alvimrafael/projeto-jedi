1354d6facd4b01a81c7515bda38da6ebe697e28c
Rafael Alvim <rafael.alvimdossantos@gmail.com>
2022-02-17 16:33:32 -0300

    exercicio_04

---
+++ src/conquista_01/exercicio_04.java
@@ -0,0 +1,18 @@
+package conquista_01;
+
+// Criar um código que declara e inicializa variáveis do tipo primitivo dentro do método main.
+
+public class exercicio_04 {
+
+    public static void main (String[] args) {
+
+        byte a = 123;
+        short b = 12321;
+        char c = 'o';
+        int d = 123321;
+        long e = -87654321;
+        float f = 12.3f;
+        double g = 12.3;
+        boolean h = false;
+    }
+}
