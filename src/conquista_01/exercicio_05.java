83a4b528909313db07c9db2e2b693e8652086bfc
Rafael Alvim <rafael.alvimdossantos@gmail.com>
2022-02-17 17:41:27 -0300

    exercicio_05

---
+++ src/conquista_01/exercicio_05.java
@@ -0,0 +1,19 @@
+package conquista_01;
+
+public class exercicio_05 {
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 2;
+
+        int sum = a + b;
+        int minus = a - b;
+        int multi = a * b;
+        int div = a / b;
+        int rest = a % b;
+
+        System.out.printf("Sum = %d%n Minus = %d%n Multiplication = %d%n Division = %d%n Rest = %d%n",
+                sum, minus, multi, div, rest);
+
+    }
+}
